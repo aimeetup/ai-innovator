ebac82a01ad1485e49a3ff05009b490cd0ce2194
stefian <stefan.ianta@w3ai.net>
2020-05-27 21:19:52 -0300

    added url for user status update and fetch config

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -82,7 +82,16 @@ class Feed extends Component {
 
   statusUpdateHandler = event => {
     event.preventDefault();
-    fetch('URL')
+    fetch('http://localhost:8080/auth/status', {
+      method: 'PATCH',
+      headers: {
+        Authorization: 'Bearer ' + this.props.token,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        status: this.state.status
+      })
+    })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
           throw new Error("Can't update status!");
