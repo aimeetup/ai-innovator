385b711c92649948543c879653b3f75589b5b053
stefian <stefan.ianta@w3ai.net>
2020-05-28 14:56:22 -0300

    added updatePost method and else if for data.action === update

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -43,7 +43,9 @@ class Feed extends Component {
     const socket = openSocket('http://localhost:8080');  // Will get Client Connected if consoled log on the Node server
     socket.on('posts', data => {  // listening to the 'posts' events / channel
       if (data.action === 'create') {
-        this.addPost(data.post)
+        this.addPost(data.post);
+      } else if (data.action === 'update') {
+        this.updatePost(data.post);
       }
     });
   }
@@ -60,6 +62,19 @@ class Feed extends Component {
       return {
         posts: updatedPosts,
         totalPosts: prevState.totalPosts + 1
+      };
+    });
+  };
+
+  updatePost = post => {
+    this.setState(prevState => {
+      const updatedPosts = [...prevState.posts];
+      const updatedPostIndex = updatedPosts.findIndex(p => p._id === post._id);
+      if (updatedPostIndex > -1) {
+        updatedPosts[updatedPostIndex] = post;
+      }
+      return {
+        posts: updatedPosts
       };
     });
   };
@@ -184,15 +199,7 @@ class Feed extends Component {
           createdAt: resData.post.createdAt
         };
         this.setState(prevState => {
-          let updatedPosts = [...prevState.posts];
-          if (prevState.editPost) {
-            const postIndex = prevState.posts.findIndex(
-              p => p._id === prevState.editPost._id
-            );
-            updatedPosts[postIndex] = post;
-          }
           return {
-            posts: updatedPosts,
             isEditing: false,
             editPost: null,
             editLoading: false
